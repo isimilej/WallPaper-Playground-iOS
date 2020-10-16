8ca5e2760d68d68afb4d6af9dceb566a385ce8cf
60029570 <jangwon@shinhan.dev>
2020-10-16 16:55:41 +0900

    Initial Commit

---
+++ WallPaper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WallPaper
+//
+//  Created by 60029570 on 2020/10/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WallPaper/WallPaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WallPaperApp.swift
+//  WallPaper
+//
+//  Created by 60029570 on 2020/10/16.
+//
+
+import SwiftUI
+
+@main
+struct WallPaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
