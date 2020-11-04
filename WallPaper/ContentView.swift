bccf4de29c5cd0d14c098bdbb4db9060be4ffead
kwangjin <isimilej@gmail.com>
2020-11-04 14:18:59 +0900

    commit

--- WallPaper/ContentView.swift
+++ WallPaper/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        //Text("Hello, world!").padding()
+        //image - from bundle, from system icons, from UIImage
+        // sized so that it fills all the available space.
+        Image("ap9LPu").resizable()
+//        guard let img = UIImage(named: "ap9LPu") else {
+//            fatalError("Unable to load image")
+//        }
+//        return Image(uiImage: img)
     }
 }
 
